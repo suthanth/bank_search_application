91e7d0825d7433b882ee54657414db76db442915
Suthanth D K <suthuganesha.dk@gmail.com>
2019-06-28 23:24:09 +0530

    Added DB models

--- bank_search_application/bank_search_api/models.py
+++ bank_search_application/bank_search_api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class banks(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=49)
+
+    class Meta:
+        ordering = ['id']
+
+
+class branches(models.Model):
+    ifsc = models.CharField(max_length=19, primary_key=True)
+    branch = models.CharField(max_length=74)
+    address = models.CharField(max_length=195)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    state = models.CharField(max_length=26)
+    bank = models.ForeignKey(banks, on_delete=models.CASCADE, related_name='banks', null=True)
+
+    class Meta:
+        ordering = ['ifsc']
+
+    @property
+    def bank_details(self):
+        return self.bank_id
